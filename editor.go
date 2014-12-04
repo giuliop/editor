9a193c142d0854641ba2bc0851d75b88d9efea9a
giulio <giuliop@gmail.com>
2014-12-04 21:50:56 +0100

    now you can add new lines

--- editor.go
+++ editor.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+
 	"github.com/mattn/go-runewidth"
 	"github.com/nsf/termbox-go"
 )
@@ -12,13 +15,21 @@ type cursor struct {
 }
 
 type line []rune
+type direction int
+
+const (
+	up direction = iota
+	down
+	left
+	rigth
+)
+
+const defCol = termbox.ColorDefault
 
 var (
 	cs   *cursor
 	text []line
 )
-
-const defCol = termbox.ColorDefault
 
 func insertChar(ch rune) {
 	text[cs.line] = append(text[cs.line], 0)
@@ -28,13 +39,79 @@ func insertChar(ch rune) {
 	cs.viPos += runewidth.RuneWidth(ch)
 }
 
+func insertNewLineChar() {
+	insertChar('\n')
+	addNewLine(cs.line + 1)
+	cs.chPos = 0
+	cs.viPos = 0
+	cs.line += 1
+}
+
+func newLine() line {
+	return make([]rune, 0, 100)
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func debug(args ...interface{}) {
+	msg := []byte(fmt.Sprintln(args))
+	err := ioutil.WriteFile("debug", msg, 0644)
+	check(err)
+}
+
+func addNewLine(line int) {
+	oldCs := *cs
+	if line > 0 && text[line-1][len(text[line-1])-1] != '\n' {
+		cs.line = line - 1
+		cs.chPos = len(text[line-1])
+		insertChar('\n')
+	}
+	// if line to be added at the end
+	if line == oldCs.line+1 {
+		text = append(text, newLine())
+	} else {
+		text = append(text, nil)
+		copy(text[line+1:], text[line:])
+		text[line] = newLine()
+	}
+	*cs = oldCs
+}
+
+func newViPos() int {
+	v := 0
+	for _, c := range text[cs.line] {
+		v += runewidth.RuneWidth(c)
+	}
+	return v
+}
+
 func deleteChBackward() {
 	if cs.chPos == 0 {
-		return
+		if cs.line == 0 {
+			return
+		} else {
+			cs.line -= 1
+			deleteLine(cs.line + 1)
+			if text[cs.line][len(text[cs.line])-1] != '\n' {
+				panic(fmt.Sprintf("Last char of line %v is %v, was wxpecting \\n", cs.line, text[cs.line]))
+			}
+			text[cs.line] = text[cs.line][:len(text[cs.line])-1]
+			cs.chPos = len(text[cs.line])
+			cs.viPos = newViPos()
+		}
+	} else {
+		cs.chPos -= 1
+		cs.viPos -= runewidth.RuneWidth(text[cs.line][cs.chPos])
+		text[cs.line] = append(text[cs.line][:cs.chPos], text[cs.line][cs.chPos+1:]...)
 	}
-	cs.chPos -= 1
-	cs.viPos -= runewidth.RuneWidth(text[cs.line][cs.chPos])
-	text[cs.line] = append(text[cs.line][:cs.chPos], text[cs.line][cs.chPos+1:]...)
+}
+
+func deleteLine(line int) {
+	text = append(text[:line], text[line+1:]...)
 }
 
 func DeleteRuneForward() {
@@ -62,7 +139,7 @@ func main() {
 	termbox.SetInputMode(termbox.InputEsc)
 
 	text = make([]line, 1, 20)
-	text[0] = make([]rune, 0, 100)
+	text[0] = newLine()
 	cs = &cursor{0, 0, 0}
 	draw()
 
@@ -79,6 +156,8 @@ mainloop:
 				insertChar('\t')
 			case termbox.KeySpace:
 				insertChar(' ')
+			case termbox.KeyEnter, termbox.KeyCtrlJ:
+				insertNewLineChar()
 			default:
 				if ev.Ch != 0 {
 					insertChar(ev.Ch)
